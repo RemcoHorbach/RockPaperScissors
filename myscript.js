4fc3c1c0aa23a3dfe6f2948763169a93f3f28aef
RemcoHorbach <horbachremco@gmail.com>
2022-12-27 21:28:41 +0100

    added var for scores and buttons and added fucntion for computer choice

---
+++ myscript.js
@@ -0,0 +1,8 @@
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
+
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
